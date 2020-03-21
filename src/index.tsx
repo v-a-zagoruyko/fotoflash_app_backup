96a9d9488bc36ce1e100b361aa5f5eb6d2479567
zagoruykov <v.a.zagoruyko@gmail.com>
2020-03-21 11:25:07 +0500

    Prettify

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
 
-function App() {
-  return (
-    <div className="App"></div>
-  );
+class App extends React.Component {
+  render() {
+    return <div className="App">123</div>;
+  }
 }
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 serviceWorker.unregister();
