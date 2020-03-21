4af15a9ebdb6fbf1c69b64f393f6e607cdbb5126
zagoruykov <v.a.zagoruyko@gmail.com>
2020-03-21 21:31:33 +0500

    Implement default page

---
+++ src/pages/Default/Default.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import cn from "classnames/bind";
+import { Button } from "components";
+import { history } from "utils";
+import styles from "./Default.module.scss";
+
+const cx = cn.bind(styles);
+
+class Default extends React.Component {
+  redirectToHome = () => {
+    history.push("/");
+  };
+
+  render() {
+    return (
+      <section className={cx("container")}>
+        <h1 className={cx("container--title")}>
+          <span>Страница</span>
+          <span>не найдена</span>
+        </h1>
+        <Button onClick={this.redirectToHome} color="primary" design="fill">
+          На главную
+        </Button>
+      </section>
+    );
+  }
+}
+
+export default Default;
