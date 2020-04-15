615c2d71c459bfac3e30800c9fa1558ef924b3ce
zagoruykov <v.a.zagoruyko@gmail.com>
2020-04-15 17:30:24 +0500

    Updated default page

--- src/pages/Default/Default.tsx
+++ src/pages/Default/Default.tsx
@@ -1,27 +1,9 @@
 import React from "react";
-import cn from "classnames/bind";
-import { Button } from "components";
-import styles from "./Default.module.scss";
-
-const cx = cn.bind(styles);
+import { Redirect } from "react-router-dom";
 
 class Default extends React.Component {
-  redirectToHome = () => {
-    window.location.replace("/");
-  };
-
   render() {
-    return (
-      <section className={cx("container")}>
-        <h1 className={cx("container--title")}>
-          <span>Страница</span>
-          <span>не найдена</span>
-        </h1>
-        <Button onClick={this.redirectToHome} color="primary" design="fill">
-          На главную
-        </Button>
-      </section>
-    );
+    return <Redirect to="/" />;
   }
 }
 
