7d28e848517e324eaefce0727d549c2743e87a32
zagoruykov <v.a.zagoruyko@gmail.com>
2020-04-15 17:29:56 +0500

    Updated general layout

--- src/layouts/General/General.tsx
+++ src/layouts/General/General.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import Media from "react-media";
 import { Helmet } from "react-helmet";
+import * as Skelet from "react-loading-skeleton";
 import cn from "classnames/bind";
+import { NavDesktop, NavMobile, Footer } from "components";
 import styles from "./General.module.scss";
 
 const cx = cn.bind(styles);
@@ -13,6 +16,61 @@ interface Props {
 interface State {}
 
 class General extends React.Component<Props, State> {
+  Loading = () => {
+    return (
+      <aside className={cx("grid")}>
+        <div className={cx("grid--item")}>
+          <h6 className={cx("title")}>
+            <Skelet.default />
+          </h6>
+          <p className={cx("caption")}>
+            <Skelet.default count={5} />
+          </p>
+        </div>
+        <div className={cx("grid--item")}>
+          <h6 className={cx("title")}>
+            <Skelet.default />
+          </h6>
+          <p className={cx("text")}>
+            <Skelet.default count={5} />
+          </p>
+        </div>
+        <div className={cx("grid--item")}>
+          <h6 className={cx("title")}>
+            <Skelet.default />
+          </h6>
+          <p className={cx("text")}>
+            <Skelet.default count={5} />
+          </p>
+        </div>
+        <div className={cx("grid--item")}>
+          <h6 className={cx("title")}>
+            <Skelet.default />
+          </h6>
+          <p className={cx("text")}>
+            <Skelet.default count={5} />
+          </p>
+        </div>
+        <div className={cx("grid--item")}>
+          <h6 className={cx("title")}>
+            <Skelet.default />
+          </h6>
+          <p className={cx("text")}>
+            <Skelet.default count={5} />
+          </p>
+        </div>
+        <div className={cx("grid--item")}>
+          <h6 className={cx("title")}>
+            <Skelet.default />
+          </h6>
+          <p className={cx("text")}>
+            <Skelet.default count={5} />
+          </p>
+        </div>
+      </aside>
+    );
+  };
+
   render() {
     const { title, children, isLoading } = this.props;
 
@@ -22,9 +80,13 @@ class General extends React.Component<Props, State> {
           <title>{`FOTOFLASH.studio - ${title}`}</title>
         </Helmet>
         <section className={cx("container")}>
-          <article className={cx("container--content")}>
-            {isLoading ? null : children}
+          <Media queries={{ small: { maxWidth: 992 } }}>
+            {matches => (matches.small ? <NavMobile /> : <NavDesktop />)}
+          </Media>
+          <article className={cx("general-container")}>
+            {isLoading ? this.Loading() : children}
           </article>
+          <Footer />
         </section>
       </>
     );
